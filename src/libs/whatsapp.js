84d82a1d5e77714a9df73eb76fc9674f4319e297
deaaprizal <dea.afrizal@icloud.com>
2021-12-17 08:04:46 +0700

    fix phone

--- src/libs/whatsapp.js
+++ src/libs/whatsapp.js
@@ -16,5 +16,5 @@ export const whatsappDemo = (
   phone,
   city
 ) => {
-  return (window.location.href = `${path_demo}${phone}&text=Hi%20min!%2C%20saya%20mau %20beli%20produk%20${title}%0A%0Acode:%20${code}%0Aharga:%20${price}%0Aukuran:%20${size}%0Awarna:%20${color}%0A%0Anama pembeli:%20${buyer}%0Ano.tlp:%20${phone}%0Adikirim%20ke:%20${city}%0A%0ATerimakasih%20:)`);
+  return (window.location.href = `${path_demo}${phone}&text=Hi%20min!%2C%20saya%20mau %20beli%20produk%20${title}%0A%0Acode:%20${code}%0Aharga:%20${price}%0Aukuran:%20${size}%0Awarna:%20${color}%0A%0Anama pembeli:%20${buyer}%0Ano.tlp:%20+628123123123%0Adikirim%20ke:%20${city}%0A%0ATerimakasih%20:)`);
 };
