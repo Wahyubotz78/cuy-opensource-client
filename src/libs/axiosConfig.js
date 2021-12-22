32d095572cb0197aa46dfa133e77498d1e539e75
rizsyad <rizsyad@gmail.com>
2021-12-22 13:44:30 +0700

    update apiServices

--- src/libs/axiosConfig.js
+++ src/libs/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import config from "config";
 
 const axiosConfig = axios.create({
-  baseURL: `http://localhost:3000/v1/`,
+  baseURL: config.baseURLAPI,
   headers: {
     Authorization: `Bearer 3949b1c47a538ac303a5e06d8dc2c5c7580a72f716e1f05aa354d9c136bcd404f85dbb811fe6efbbea1dc09d0c8d36dc7e42fc5eb7960a60425ed93cf280108a86b254a83246e06631140c4f0412b52c83bfa8f4`,
     AccessControlAllowOrigin: true,
--- src/libs/request.js
+++ src/libs/request.js
@@ -2,43 +2,43 @@ import axiosConfig from "./axiosConfig";
 
 export const request = {
   // members endpoint
-  login: (data) => axiosConfig.post(`login`, data),
-  register: (data) => axiosConfig.post(`register`, data),
+  login: (data) => axiosConfig.post(`login`, { data }),
+  register: (data) => axiosConfig.post(`register`, { data }),
   getAllUser: () => axiosConfig.get(`get/members`),
-  getUserById: (data) => axiosConfig.post(`get/member/id`, data),
-  getUserByKey: (data) => axiosConfig.post(`get/member/`, data),
-  updateMember: (data) => axiosConfig.put(`update/member`, data),
-  changePhone: (data) => axiosConfig.put(`change/phone`, data),
-  changePassword: (data) => axiosConfig.put(`change/password`, data),
-  forgotPassword: (data) => axiosConfig.put(`forgot/password`, data),
-  deleteMember: (data) => axiosConfig.delete(`delete/member`, data),
+  getUserById: (data) => axiosConfig.post(`get/member/id`, { data }),
+  getUserByKey: (data) => axiosConfig.post(`get/member/`, { data }),
+  updateMember: (data) => axiosConfig.put(`update/member`, { data }),
+  changePhone: (data) => axiosConfig.put(`change/phone`, { data }),
+  changePassword: (data) => axiosConfig.put(`change/password`, { data }),
+  forgotPassword: (data) => axiosConfig.put(`forgot/password`, { data }),
+  deleteMember: (data) => axiosConfig.delete(`delete/member`, { data }),
 
   // courses endpoint
-  createCourse: (data) => axiosConfig.post(`course`, data),
-  updateCourse: (data) => axiosConfig.put(`course`, data),
+  createCourse: (data) => axiosConfig.post(`course`, { data }),
+  updateCourse: (data) => axiosConfig.put(`course`, { data }),
   getAllCourse: () => axiosConfig.get(`course/find`),
-  getCourceById: (data) => axiosConfig.post(`course/findone`, data),
-  getCourceByStatus: (data) => axiosConfig.post(`course/status`, data),
-  deleteCource: (data) => axiosConfig.delete(`course`, data),
+  getCourceById: (data) => axiosConfig.post(`course/findone`, { data }),
+  getCourceByStatus: (data) => axiosConfig.post(`course/status`, { data }),
+  deleteCource: (data) => axiosConfig.delete(`course`, { data }),
 
   // modules endpoint
-  createModule: (data) => axiosConfig.post(`module`, data),
-  updateModule: (data) => axiosConfig.put(`module`, data),
+  createModule: (data) => axiosConfig.post(`module`, { data }),
+  updateModule: (data) => axiosConfig.put(`module`, { data }),
   getAllModule: () => axiosConfig.get(`module`),
-  getModuleById: (data) => axiosConfig.post(`module/findone`, data),
-  deleteModule: (data) => axiosConfig.delete(`module`, data),
+  getModuleById: (data) => axiosConfig.post(`module/findone`, { data }),
+  deleteModule: (data) => axiosConfig.delete(`module`, { data }),
 
   // members-course endpoint
-  createMemberCourse: (data) => axiosConfig.post(`member-course`, data),
-  updateMemberCourse: (data) => axiosConfig.put(`member-course`, data),
+  createMemberCourse: (data) => axiosConfig.post(`member-course`, { data }),
+  updateMemberCourse: (data) => axiosConfig.put(`member-course`, { data }),
   getMemberCourseById: (data) =>
-    axiosConfig.post(`member-course/findone`, data),
-  deleteMemberCourse: (data) => axiosConfig.delete(`member-course`, data),
+    axiosConfig.post(`member-course/findone`, { data }),
+  deleteMemberCourse: (data) => axiosConfig.delete(`member-course`, { data }),
 
   // orders endpoint
-  createOrder: (data) => axiosConfig.post(`order/create`, data),
-  getOrderById: (data) => axiosConfig.post(`order/findone`, data),
-  updateOrder: (data) => axiosConfig.put(`order/status`, data),
-  updateOrderSimulate: (data) => axiosConfig.put(`order/simulate`, data),
-  getAllOrderByMember: (data) => axiosConfig.post(`order/member`, data),
+  createOrder: (data) => axiosConfig.post(`order/create`, { data }),
+  getOrderById: (data) => axiosConfig.post(`order/findone`, { data }),
+  updateOrder: (data) => axiosConfig.put(`order/status`, { data }),
+  updateOrderSimulate: (data) => axiosConfig.put(`order/simulate`, { data }),
+  getAllOrderByMember: (data) => axiosConfig.post(`order/member`, { data }),
 };
